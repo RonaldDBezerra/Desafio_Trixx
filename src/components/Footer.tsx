4d25de8d672216bd2e6d293eed5e8d0783933edb
RonaldDBezerra <rebulgame@gmail.com>
2023-07-23 12:55:36 -0300

    Feat: Add component Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { View } from 'react-native';
+import Styles from '../styles/Styles';
+
+export function Footer() {
+  return <View style={Styles.footer}></View>;
+}
